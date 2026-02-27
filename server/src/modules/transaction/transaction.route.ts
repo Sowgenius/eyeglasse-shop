a10e99bf0d5f1739120b627c37269318c657aee5
Sowgenius <korkasow1@gmail.com>
2026-02-27 18:35:08 +0000

    fix: add transactions endpoint to server

---
+++ server/src/modules/transaction/transaction.route.ts
@@ -0,0 +1,34 @@
+import { verifyToken } from '@/middlewares/auth';
+import { catchAsync } from '@/utils';
+import { sendResponse } from '@/utils/send-response';
+import { Router } from 'express';
+import { prisma } from '@/lib/prisma';
+
+const router = Router();
+
+router.get(
+  '/',
+  verifyToken(),
+  catchAsync(async (req, res) => {
+    const userId = req.jwtPayload.userId;
+    
+    const transactions = await prisma.product.findMany({
+      where: {
+        userId,
+      },
+      select: {
+        _count: {
+          select: {
+            stockMovements: true,
+          },
+        },
+      },
+    });
+
+    return sendResponse(res, {
+      data: transactions,
+    });
+  })
+);
+
+export const TransactionRoutes = router;
